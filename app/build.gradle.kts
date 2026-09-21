15608e1de362849c58b62d1a679e8362f609b20c
kurashiro-aria <eduardo.rojas.a96@gmail.com>
2026-09-21 06:22:57 +0000

    Fix JVM 17 compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,11 @@ plugins {
 }
 
 android {
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     namespace = "com.kura.aria"
     compileSdk = 35
 
