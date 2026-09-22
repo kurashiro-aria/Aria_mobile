4b298d4a1b36d71bef3533723d60bbfa57923c8f
kurashiro-aria <eduardo.rojas.a96@gmail.com>
2026-09-22 16:33:07 -0300

    build: bump ARIA Alpha to 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.kura.aria"
         minSdk = 33
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.3-alpha"
+        versionCode = 6
+        versionName = "0.2.4-alpha"
     }
 }
 
