54262d66afb1eec641c455f828fc9a9612ce89fb
kurashiro-aria <eduardo.rojas.a96@gmail.com>
2026-09-22 22:36:27 -0300

    build: prepare ARIA 0.2.11 interface alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val ariaVersionName = "0.2.10-alpha"
+val ariaVersionName = "0.2.11-alpha"
 
 android {
     compileOptions {
@@ -36,7 +36,7 @@ android {
         applicationId = "com.kura.aria"
         minSdk = 33
         targetSdk = 35
-        versionCode = 12
+        versionCode = 13
         versionName = ariaVersionName
         resValue("string", "app_name", "ARIA Mobile $ariaVersionName")
     }
