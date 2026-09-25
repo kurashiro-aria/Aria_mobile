664e31ccfe85b4b5dd93ebbcad2125bdf4739248
kurashiro-aria <eduardo.rojas.a96@gmail.com>
2026-09-25 20:26:34 -0300

    Release ARIA Mobile 0.2.36 alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val ariaVersionName = "0.2.35-alpha"
+val ariaVersionName = "0.2.36-alpha"
 
 android {
     compileOptions {
@@ -36,7 +36,7 @@ android {
         applicationId = "com.kura.aria"
         minSdk = 33
         targetSdk = 35
-        versionCode = 42
+        versionCode = 43
         versionName = ariaVersionName
         resValue("string", "app_name", "ARIA Mobile $ariaVersionName")
     }
